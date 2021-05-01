607dea3720cd29ba4825837083aa1b11b9a9d909
amartya <amartyathorat@gmail.com>
2021-05-01 18:46:25 +0530

    store register

--- src/store/equitiy.module.js
+++ src/store/equitiy.module.js
@@ -1,36 +1,78 @@
+import {getEquties,exportEquities}  from "@/common/api.service";
+
 const state = {
-	page:0,
-	limit:100,
-	isLoading:true,
-	equitiesCount:0,
-	equitits:[],
+	date:undefined,
+	count:0,
+	equities:[],
 	nextPage:false,
-	prevPage:false
+	prevPage:false,
+	isLoading:false,
+	selected:new Set()
 };
 
 const getters = {
-	equitiesCount(state){
-		return state.equitiesCount;
-	}
-	equitits(state){
-		return state.equitits;
-	}
+
+	date(state){
+		return state.date
+	},
+	equities(state){
+		return state.equities
+	},
+	selected(state){
+		return state.selected
+	},
+	nextPage(state){
+		return state.nextPage
+	},
+	prevPage(state){
+		return state.prevPage
+	},
+	count(state){
+		return state.count
+	},
 	isLoading(state){
 		return state.isLoading;
-	}
-	isNextPage(state){
-		return state.nextPage
-	}
-	isPrevPage(state){
-		return state.prevPage
 	}
 };
 
 const actions = {
-	fetchEquities()
 
+	fetch({commit},data){
+		commit('startFetch');
+		return getEquties(data)
+			.then((data) => {
+				commit('endFetch',data);
+			})
+			.catch(error=>{
+				console.log(error);
+			})
+	},
+	exporte({state},date){
 
+		return exportEquities(date,Array.from(state.selected))	
+	}
+ }
+
+const mutations={
+	startFetch(state){
+		state.isLoading = true;
+	},
+	endFetch(state,result){
+		state.count = result['count']
+		state.equities = result['equities']
+		state.nextPage = result['next_page']
+		state.date = result['date']
+		state.isLoading = false		
+	},
+	selectEquity(state,equity){
+		state.selected.add(equity)
+	}
 }
 
 
-
+export default {
+	state,
+	getters,
+	actions,
+	mutations
+}
